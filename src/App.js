afde7e945d08b8df05133ecb10f7f0ff7fdea109
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-07-14 07:22:13 +0300

    Correcting the comment with one chaild component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => {
-  return (
-    <div className="Math-Calculator">
-      <div className="result-display">
-        <span className="result">0</span>
-      </div>
-      <Calculator />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,63 +1,13 @@
+import React from 'react';
+import ButtonCalculator from './ButtonCalc';
+
 const Calculator = () => {
   return (
-    <div className="keyButtons">
-      <button data="AC" className="btn" type="button">
-        AC
-      </button>
-      <button data="+/-" className="btn" type="button">
-        +/-
-      </button>
-      <button data="%" className="btn" type="button">
-        %
-      </button>
-      <button data="/" className="btn-operator" type="button">
-        &divide;
-      </button>
-      <button data="7" className="btn" type="button">
-        7
-      </button>
-      <button data="8" className="btn" type="button">
-        8
-      </button>
-      <button data="9" className="btn" type="button">
-        9
-      </button>
-      <button data="*" className="btn-operator" type="button">
-        &times;
-      </button>
-      <button data="4" className="btn" type="button">
-        4
-      </button>
-      <button data="5" className="btn" type="button">
-        5
-      </button>
-      <button data="6" className="btn" type="button">
-        6
-      </button>
-      <button data="-" className="btn-operator" type="button">
-        &ndash;
-      </button>
-      <button data="1" className="btn" type="button">
-        1
-      </button>
-      <button data="2" className="btn" type="button">
-        2
-      </button>
-      <button data="3" className="btn" type="button">
-        3
-      </button>
-      <button data="+" className="btn-operator" type="button">
-        +
-      </button>
-      <button data="0" className="btn btn-key" type="button">
-        0
-      </button>
-      <button data="." className="btn" type="button">
-        .
-      </button>
-      <button data="=" className="btn-operator" type="button">
-        =
-      </button>
+    <div className="Math-Calculator">
+      <div className="result-display">
+        <span className="result">0</span>
+      </div>
+      <ButtonCalculator />
     </div>
   );
 };
